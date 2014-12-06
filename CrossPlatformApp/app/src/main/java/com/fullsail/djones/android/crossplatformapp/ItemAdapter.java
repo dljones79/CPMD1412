a22c66f3eaa1ddc82635a7561298bd2265261613
David Jones <dljones79@fullsail.edu>
2014-12-06 14:12:21 -0600

    ListView being populated from Parse

---
+++ CrossPlatformApp/app/src/main/java/com/fullsail/djones/android/crossplatformapp/ItemAdapter.java
@@ -0,0 +1,61 @@
+package com.fullsail.djones.android.crossplatformapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by David on 12/6/14.
+ */
+public class ItemAdapter extends BaseAdapter {
+
+    private static final long ID_CONSTANT = 0x01000000;
+
+    Context mContext;
+    ArrayList<ParseObject> mItems;
+
+    public ItemAdapter(Context context, ArrayList<ParseObject> items){
+        mContext = context;
+        mItems = items;
+    }
+
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return ID_CONSTANT + position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.listview_item, parent, false);
+        }
+
+        ParseObject parseObject = (ParseObject) getItem(position);
+        Integer qty = parseObject.getInt("quantity");
+        String quantity = qty.toString();
+        TextView itemNameView = (TextView) convertView.findViewById(R.id.itemText);
+        TextView quantityNameView = (TextView) convertView.findViewById(R.id.quantityText);
+        itemNameView.setText(parseObject.getString("item"));
+        quantityNameView.setText(quantity);
+
+        return convertView;
+    }
+}
--- CrossPlatformApp/app/src/main/java/com/fullsail/djones/android/crossplatformapp/ViewFragment.java
+++ CrossPlatformApp/app/src/main/java/com/fullsail/djones/android/crossplatformapp/ViewFragment.java
@@ -1,11 +1,21 @@
 package com.fullsail.djones.android.crossplatformapp;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +23,8 @@ import android.view.ViewGroup;
  */
 public class ViewFragment extends Fragment {
 
+    ArrayList<String> items;
+    ArrayAdapter<String> listAdapter;
 
     public ViewFragment() {
         // Required empty public constructor
@@ -26,5 +38,29 @@ public class ViewFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_view, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        /*
+        ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(getActivity(), "Item");
+        adapter.setTextKey("item");
+
+        ListView listView = (ListView)getActivity().findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        */
+
+        ParseQuery<ParseObject> pQuery = ParseQuery.getQuery("Item");
+        pQuery.whereExists("item");
+        pQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                ListView listView = (ListView) getView().findViewById(R.id.listView);
+                ItemAdapter itemAdapter = new ItemAdapter(getActivity(), (ArrayList<ParseObject>) parseObjects);
+                listView.setAdapter(itemAdapter);
+            }
+        });
+
+    }
 
 }
